09e83b28e2e3135a3713d0385a6f618e54152186
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-04 21:03:46 -0300

    array of String

---
+++ src/main/kotlin/one/dio/collections/TestDoubleArray.kt
@@ -0,0 +1,17 @@
+package one.dio.collections
+
+import kotlin.math.round
+
+fun main(){
+    val fees = doubleArrayOf(
+        1000.0,
+        2000.0,
+        1800.0
+    )
+
+    fees.forEachIndexed { index, salary ->
+        fees[index] = salary * 1.1
+    }
+
+    fees.forEach { println(round(it)) }
+}
