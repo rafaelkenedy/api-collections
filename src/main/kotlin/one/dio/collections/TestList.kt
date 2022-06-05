7653bd160cf12fbdad0eed1d33926bab4df86e77
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-04 21:44:05 -0300

    group by test

--- src/main/kotlin/one/dio/collections/TestList.kt
+++ src/main/kotlin/one/dio/collections/TestList.kt
@@ -4,8 +4,10 @@ import kotlin.math.round
 
 fun main() {
 
-    val rafael = Employee("Rafael", 5000.0)
-    val danielle = Employee("Danielle", 4500.0)
+    //Imutable List
+    val rafael = Employee("Rafael", 5000.0, "MALE")
+    val danielle = Employee("Danielle", 4500.0, "FEMALE")
+    val daniel = Employee("Daniel", 3500.0, "MALE")
 
     val employees = listOf(rafael, danielle)
     employees.forEach{ println(it) }
@@ -17,15 +19,21 @@ fun main() {
     employees
         .sortedBy { it.salary }
         .forEach { println(it) }
+
+    println("Sorted by gender:")
+    employees
+        .groupBy { it.gender}
+        .forEach { println(it) }
 }
 
 data class Employee(
     val name: String,
-    val salary: Double
+    val salary: Double,
+    val gender: String
 ) {
     override fun toString(): String =
         """
             Name: $name
-            Salary: $salary
+            Salary: $salary            
         """.trimIndent()
 }
