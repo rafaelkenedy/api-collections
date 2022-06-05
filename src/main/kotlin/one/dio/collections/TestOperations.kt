09710640e99f923f38a6df6890e5b4bb72df5e4b
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-04 21:20:54 -0300

    count, find and any test

--- src/main/kotlin/one/dio/collections/TestOperations.kt
+++ src/main/kotlin/one/dio/collections/TestOperations.kt
@@ -7,7 +7,8 @@ fun main() {
         1200.0,
         2300.0,
         1850.0,
-        2550.0
+        2550.0,
+        2560.0,
     )
 
     fees.forEach { println(round(it)) }
@@ -17,4 +18,9 @@ fun main() {
 
     val salaryBiggerThen2500 = fees.filter { it > 2500.0 }
     salaryBiggerThen2500.forEach { println(it) }
+
+    println(fees.count { it in 2000.0..5000.0 })
+    println(fees.find { it === 1850.0 })
+    println(fees.any { it === 1850.0 })
+    println(fees.any { it === 300.0 })
 }
